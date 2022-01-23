a19d126cdc8679d4390633dfa8eb40ab662d1cbd
lonnalu <lonnalu@gmail.com>
2022-01-23 14:52:57 -0800

    Updates html title and loading message

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -3,7 +3,7 @@ import classes from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={classes.container}>
-      <p>Loading...</p>
+      <p>Entering The MET...</p>
       <img src='loading-svgrepo-com.svg' alt='loading icon' className={classes.loader} />
     </div>
   )
