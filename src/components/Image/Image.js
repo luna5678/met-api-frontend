7f3a43aa25b38aa5b891697bc4a651dc5d5284f4
lonnalu <lonnalu@gmail.com>
2022-01-24 11:06:55 -0800

    Improves class names

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,7 +3,7 @@ import classes from './Image.module.css'
 const Image = (props) => {
   return (
     <a href={props.link} target='_blank' rel='noreferrer'>
-      <img src={props.image} alt={`${props.title} by ${props.artist}`} />
+      <img src={props.image} alt={`${props.title} by ${props.artist}`} className={classes.image} />
     </a>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,9 @@ import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={classes.navbar}>
       <a href='https://www.metmuseum.org/' target='_blank' rel="noreferrer">
-        <img src='the-met-logo.png' alt='The MET logo' />
+        <img src='the-met-logo.png' alt='The MET logo' className={classes.logo} />
       </a>
     </nav>
   )
