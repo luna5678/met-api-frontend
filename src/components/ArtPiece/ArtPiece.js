3968941af9d6b552953db20a8bad0346f9e51bd0
lonnalu <lonnalu@gmail.com>
2022-01-24 10:44:14 -0800

    Adds link to view details to rendered image

--- src/components/ArtPiece/ArtPiece.js
+++ src/components/ArtPiece/ArtPiece.js
@@ -8,6 +8,7 @@ const ArtPiece = () => {
   const [title, setTitle] = useState('');
   const [artist, setArtist] = useState('');
   const [image, setImage] = useState('');
+  const [fullLink, setFullLink] = useState('');
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -22,6 +23,7 @@ const ArtPiece = () => {
       setTitle(data.title);
       setArtist(data.artistDisplayName);
       setImage(data.primaryImage);
+      setFullLink(data.objectURL);
     } catch (error) {
       console.log(error.message);
       setError(true);
@@ -40,7 +42,7 @@ const ArtPiece = () => {
       {error && !isLoading && <p className={classes.error}>Oops! An error has occurred. Please try again later.</p>}
       {!error && !isLoading &&
         <>
-          <Image title={title} artist={artist} image={image} />
+          <Image title={title} artist={artist} image={image} link={fullLink}  />
           <Description title={title} artist={artist} />
         </>
       }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,7 +2,9 @@ import classes from './Image.module.css'
 
 const Image = (props) => {
   return (
-    <img src={props.image} alt={`${props.title} by ${props.artist}`} />
+    <a href={props.link} target='_blank' rel='noreferrer'>
+      <img src={props.image} alt={`${props.title} by ${props.artist}`} />
+    </a>
   )
 }
 
